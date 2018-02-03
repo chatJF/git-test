3a6b46c15d218a332a31416dfbe5df0379ef8d93
chatJF <janahitha_cf@yahoo.com>
2018-02-03 20:17:18 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+//Express middleware  any requests to /assets will be mapped to 'assets' folder - serves static files
+app.use('/assets', express.static('assets'));
+
+app.get('/', function(req, res){
+    //res.render('home')
+    res.send('hello');
+});
+
+app.listen(3000);
+console.log('listening to port 3000')
